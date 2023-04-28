fb8b6120ac154369c52f8d9bd99fbe94b13b9823
raaedahmed23 <raaedahmed23@gmail.com>
2023-04-27 22:57:31 -0500

    first draft

--- code/MIXTURE_CLT.py
+++ code/MIXTURE_CLT.py
@@ -11,7 +11,7 @@ class MIXTURE_CLT():
     def __init__(self):
         self.n_components = 0 # number of components
         self.mixture_probs = None # mixture probabilities
-        self.clt_list =[]   # List of Tree Bayesian networks
+        self.clt_list = []   # List of Tree Bayesian networks
         
 
     '''
@@ -19,22 +19,45 @@ class MIXTURE_CLT():
     '''
     def learn(self, dataset, n_components=2, max_iter=50, epsilon=1e-5):
         # For each component and each data point, we have a weight
-        weights=np.zeros((n_components,dataset.shape[0]))
+        weights = np.zeros((n_components,dataset.shape[0]))
+        self.n_components = n_components
 
         # Randomly initialize the chow-liu trees and the mixture probabilities
         # Your code for random initialization goes here
+        mix_probs = np.random.random(n_components)
+        mix_probs /= mix_probs.sum()
+        self.mixture_probs = mix_probs
+
+        self.clt_list = [CLT() for _ in range(n_components)]
+        for clt in self.clt_list:
+            clt.learn(dataset)
 
         for itr in range(max_iter):
-            #E-step: Complete the dataset to yield a weighted dataset
-            # We store the weights in an array weights[ncomponents,number of points]
-            
-            #Your code for E-step here
-            
-            # M-step: Update the Chow-Liu Trees and the mixture probabilities
-            
-            #Your code for M-Step here
+            for k in range(n_components):
 
-        
+                #E-step: Complete the dataset to yield a weighted dataset
+                # We store the weights in an array weights[ncomponents,number of point
+                #Your code for E-step here
+
+                # getProb i.e weight for each sample
+                T = np.zeros((n_components, dataset.shape[0]))
+                for i, sample in enumerate(dataset):
+                    T[k][i] = self.clt_list[k].getProb(sample)
+
+                # denominator = np.sum([np.multiply(self.mixture_probs[k], T[k]) for k in range(n_components)])
+                weights[k] = np.multiply(self.mixture_probs[k], T[k]) / np.sum(np.multiply(self.mixture_probs[k], T[k]))
+
+                # M-step: Update the Chow-Liu Trees and the mixture probabilities
+                #Your code for M-Step here
+
+                self.clt_list[k].update(dataset, weights[k])
+
+            if itr == 0:
+                curr_ll = self.computeLL(dataset) / dataset.shape[0]
+            else:
+                new_ll = self.computeLL(dataset) / dataset.shape[0]
+                if abs(new_ll - curr_ll) < epsilon:
+                    break
     
     """
         Compute the log-likelihood score of the dataset
@@ -45,6 +68,11 @@ class MIXTURE_CLT():
         #   Hint:   Likelihood of a data point "x" is sum_{c} P(c) T(x|c)
         #           where P(c) is mixture_prob of cth component and T(x|c) is the probability w.r.t. chow-liu tree at c
         #           To compute T(x|c) you can use the function given in class CLT
+        l = 0.0
+        for sample in range(dataset.shape[0]):
+            for k in range(self.n_components):
+                l += np.multiply(self.mixture_probs[k], self.clt_list[k].getProb(dataset[sample]))
+            ll += np.log(l)
         return ll
     
 
@@ -63,8 +91,16 @@ class MIXTURE_CLT():
     mix_clt.computeLL(dataset)/dataset.shape[0]
 '''
 
-    
-    
+mix_clt=MIXTURE_CLT()
+ncomponents=10 #number of components
+max_iter=50 #max number of iterations for EM
+epsilon=1e-1 #converge if the difference in the log-likelihods between two iterations is smaller 1e-1
+dataset=Util.load_dataset('../dataset/accidents.ts.data')
+mix_clt.learn(dataset,ncomponents,max_iter,epsilon)
+
+# To compute average log likelihood of a dataset w.r.t. the mixture, you can use
+dataset=Util.load_dataset('../dataset/accidents.test.data')
+mix_clt.computeLL(dataset)/dataset.shape[0]
 
 
     
