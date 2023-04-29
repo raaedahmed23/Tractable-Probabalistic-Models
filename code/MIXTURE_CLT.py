4b32f28d5026702592676fe2d7c9ab19d08ac305
raaedahmed23 <raaedahmed23@gmail.com>
2023-04-28 19:53:08 -0500

    completed mixture_clt and validation

--- code/MIXTURE_CLT.py
+++ code/MIXTURE_CLT.py
@@ -28,6 +28,9 @@ class MIXTURE_CLT():
         mix_probs /= mix_probs.sum()
         self.mixture_probs = mix_probs
 
+        # initializing variable to store Chow-Liu probabilities
+        T = np.zeros((n_components, dataset.shape[0]))
+
         self.clt_list = [CLT() for _ in range(n_components)]
         for clt in self.clt_list:
             clt.learn(dataset)
@@ -36,19 +39,19 @@ class MIXTURE_CLT():
             for k in range(n_components):
 
                 #E-step: Complete the dataset to yield a weighted dataset
-                # We store the weights in an array weights[ncomponents,number of point
-                #Your code for E-step here
+                # We store the weights in an array weights[ncomponents,number of points]
+                # Your code for E-step here
 
                 # getProb i.e weight for each sample
-                T = np.zeros((n_components, dataset.shape[0]))
                 for i, sample in enumerate(dataset):
                     T[k][i] = self.clt_list[k].getProb(sample)
-
+                # T[k] = np.array([self.clt_list[k].getProb(sample) for sample in dataset])
+                
                 # denominator = np.sum([np.multiply(self.mixture_probs[k], T[k]) for k in range(n_components)])
                 weights[k] = np.multiply(self.mixture_probs[k], T[k]) / np.sum(np.multiply(self.mixture_probs[k], T[k]))
 
                 # M-step: Update the Chow-Liu Trees and the mixture probabilities
-                #Your code for M-Step here
+                # Your code for M-Step here
 
                 self.clt_list[k].update(dataset, weights[k])
 
@@ -57,7 +60,8 @@ class MIXTURE_CLT():
             else:
                 new_ll = self.computeLL(dataset) / dataset.shape[0]
                 if abs(new_ll - curr_ll) < epsilon:
-                    break
+                    return
+                curr_ll = new_ll
     
     """
         Compute the log-likelihood score of the dataset
@@ -91,16 +95,40 @@ class MIXTURE_CLT():
     mix_clt.computeLL(dataset)/dataset.shape[0]
 '''
 
-mix_clt=MIXTURE_CLT()
-ncomponents=10 #number of components
-max_iter=50 #max number of iterations for EM
-epsilon=1e-1 #converge if the difference in the log-likelihods between two iterations is smaller 1e-1
-dataset=Util.load_dataset('../dataset/accidents.ts.data')
-mix_clt.learn(dataset,ncomponents,max_iter,epsilon)
+if __name__ == '__main__':
 
-# To compute average log likelihood of a dataset w.r.t. the mixture, you can use
-dataset=Util.load_dataset('../dataset/accidents.test.data')
-mix_clt.computeLL(dataset)/dataset.shape[0]
+    # dataset_list = ['accidents', 'baudio', 'bnetflix', 'jester', 'kdd', 'msnbc', 'nltcs', 'plants', 'pumsb_star', 'tretail']
+    dataset_list = ['accidents']
+    k_values = [2, 5, 10]
+    ll_vals = list()
+    for dataset_name in dataset_list:
+        for k in k_values:
+            mix = MIXTURE_CLT()
+
+            training = '../dataset/' + dataset_name + '.ts.data'
+            dataset=Util.load_dataset(training)
+            mix.learn(dataset, n_components= k, max_iter=1, epsilon= 1e-1)
+
+            validation = training = '../dataset/' + dataset_name + '.valid.data'
+            valid = Util.load_dataset(validation)
+
+            ll_vals.append(mix.computeLL(valid)/valid.shape[0])
+
+        ll = np.asarray(ll_vals)
+        print(f'K for {dataset_name}: {k_values[np.argmin(ll)]}')
+
+
+
+    # mix_clt=MIXTURE_CLT()
+    # ncomponents=10 #number of components
+    # max_iter=50 #max number of iterations for EM
+    # epsilon=1e-1 #converge if the difference in the log-likelihods between two iterations is smaller 1e-1
+    # dataset=Util.load_dataset('../dataset/accidents.ts.data')
+    # mix_clt.learn(dataset,ncomponents,max_iter,epsilon)
+
+    # # To compute average log likelihood of a dataset w.r.t. the mixture, you can use
+    # dataset=Util.load_dataset('../dataset/accidents.test.data')
+    # mix_clt.computeLL(dataset)/dataset.shape[0]
 
 
     
